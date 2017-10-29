5eb7bce958bd795c806b740b4a8eeb167bc25f9d
Taiwo Tejuosho <taiwotejuosho@Taiwos-MacBook-Pro.local>
2017-10-29 18:50:46 -0500

    ok1

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,17 @@
-var friendsArray = require('../data/friendsData.js');
+var friend = require('../data/friendsData.js');
 var fs = require("fs");
 
 function apiRoutesListeners (app) {
   // define the get api/friends route
   app.get('/api/friends', function(req, res) {
-      res.json(friendsArray);
+      res.json(friend);
   });
 
 
   app.post('/api/friends', function(req, res) {
           var newFriend = req.body;
-          friendsArray.push(newFriend);
+
+          friend.push(newFriend);
           fs.appendFile("friendsData.txt", newFriend, function(err){
               if (err) throw error;
               console.log("new friend added to friends Data array")
